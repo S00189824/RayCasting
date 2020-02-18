b4ab3652dc6edc4315ab0000b2293b5a86967bd2
David Coleman <46888854+S00189824@users.noreply.github.com>
2020-02-18 17:38:13 +0000

    ray

---
+++ RayCasting/Assets/Scripts/TopDownRay.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RaySelection : MonoBehaviour {
+
+    public Transform RaySource;
+    public float MaxDistance = 100;
+    public LayerMask layerMask;
+    Ray mouseray;
+    public GameObject PrefabToSpawn;
+
+    //store data on the intersection
+    RaycastHit hitResult;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            mouseray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(mouseray,out hitResult,MaxDistance,layerMask))
+            {
+
+                Vector3 point = hitResult.point;
+
+                
+
+                Instantiate(PrefabToSpawn, point, Quaternion.identity);
+                
+
+
+            }
+        }
+	}
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawRay(mouseray);
+    }
+}
